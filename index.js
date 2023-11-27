bd15e4f7453c385c4bab6e7b510f7f770be95ef8
Dan <dhowell1027@yahoo.com>
2023-11-26 18:25:22 -0700

    objects and arrays

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+
+let employee = {
+    name: 'Dan H',
+    streetAddress: '1090 S Jackson St',
+  };
+  
+  // Function 1: updateEmployeeWithKeyAndValue
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+  }
+  
+  // Function 2: destructivelyUpdateEmployeeWithKeyAndValue
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  // Function 3: deleteFromEmployeeByKey
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  // Function 4: destructivelyDeleteFromEmployeeByKey
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
